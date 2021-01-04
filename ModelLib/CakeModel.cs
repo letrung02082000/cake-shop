96cff0a367b30c5cf994e31a49f988665b546a48
TRUNG LÊ VĂN <18120622@student.hcmus.edu.vn>
2021-01-04 23:11:09 +0700

    Add project files.

---
+++ ModelLib/CakeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    public class CakeModel
+    {
+        public int CakeId { get; set; }
+        public string CakeCode { get; set; }
+        public string CakeName { get; set; }
+        public string CakeName2 { get; set; }
+        public string CakeCat { get; set; }
+        public int CakePrice { get; set; }
+        public string CakeDesc { get; set; }
+    }
+}
---
+++ ModelLib/DatabaseAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLib
+{
+    public class DatabaseAccess
+    {
+        // Cake CRUD
+        public static List<CakeModel> LoadCake()
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = connection.Query<CakeModel>($"", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static int SaveCake(CakeModel cake)
+        {
+            using (IDbConnection connection = new SQLiteConnection(LoadConnectionString()))
+            {
+                int cakeId = connection.ExecuteScalar<int>("INSERT INTO cake(cakeId, cakeCode, cakeName, cakeName2, cakeCat, cakePrice, cakeDesc) VALUES(@CakeId, @CakeCode, @CakeName, @CakeName2, @CakeCat, @CakePrice, @CakeDesc)", cake);
+                return cakeId;
+            }
+        }
+
+        private static string LoadConnectionString(string id = "default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
